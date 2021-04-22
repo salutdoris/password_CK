9fc6343585d7effb6f7bc60c313b5f72fad46e5f
Doris Chang <salutdoris@gmail>
2021-04-22 15:35:02 +0800

    i>0 and terminatio

--- password_CK.py
+++ password_CK.py
@@ -1,13 +1,13 @@
 password_c = 'a123456'
 i = 3
-while True:
+while i > 0:
     password_a = input('請輸入密碼:')
     if password_a == password_c:
         print('登入成功!')
         break
     else:
         i = i - 1
-        print('密碼錯誤! 還有',i, '次機會')
-        if i == 0:
-            print('錯誤達三次，已無法登入。')
-            break 
+        if i > 0:
+            print('密碼錯誤! 還有',i, '次機會')
+        elif i == 0:
+            print('錯誤達三次，已無法輸入')
